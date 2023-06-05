657a452565ced0848634a04333d1eb59f91d7092
Filip Drażba <fdrazba@gmail.com>
2023-06-05 21:42:56 +0200

    Initial commit.

---
+++ src/main/java/pl/edu/pb/wi/JavaBusinessApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pb.wi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBusinessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBusinessApplication.class, args);
+    }
+
+}
